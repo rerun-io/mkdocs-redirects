b64a973d6c0069ef7ba379a869dca148131ed704
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-06-13 09:49:30 +0200

    bump version

--- setup.py
+++ setup.py
@@ -12,55 +12,55 @@ def read(fname):
 
 
 test_requirements = [
-    'pytest',
-    'black',
-    'isort',
-    'autoflake',
+    "pytest",
+    "black",
+    "isort",
+    "autoflake",
 ]
 
 release_requirements = [
-    'twine>=1.13.0',
+    "twine>=1.13.0",
 ]
 
 
 setup(
-    name='mkdocs-redirects',
-    version='1.2.0',
-    description='A MkDocs plugin for dynamic page redirects to prevent broken links.',
-    long_description=read('README.md'),
+    name="mkdocs-redirects",
+    version="1.3.0",
+    description="A MkDocs plugin for dynamic page redirects to prevent broken links.",
+    long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    keywords='mkdocs redirect',
-    url='https://github.com/datarobot/mkdocs-redirects',
-    author='Dustin Burke',
-    author_email='dustin@datarobot.com',
-    license='MIT',
-    license_files=['LICENSE'],
-    python_requires='>=3.6',
+    keywords="mkdocs redirect",
+    url="https://github.com/datarobot/mkdocs-redirects",
+    author="Dustin Burke",
+    author_email="dustin@datarobot.com",
+    license="MIT",
+    license_files=["LICENSE"],
+    python_requires=">=3.6",
     install_requires=[
-        'mkdocs>=1.1.1',
+        "mkdocs>=1.1.1",
     ],
     extras_require={
-        'dev': test_requirements + release_requirements,
-        'test': test_requirements,
-        'release': release_requirements,
+        "dev": test_requirements + release_requirements,
+        "test": test_requirements,
+        "release": release_requirements,
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages(),
     entry_points={
-        'mkdocs.plugins': [
-            'redirects = mkdocs_redirects.plugin:RedirectPlugin',
+        "mkdocs.plugins": [
+            "redirects = mkdocs_redirects.plugin:RedirectPlugin",
         ]
     },
 )
